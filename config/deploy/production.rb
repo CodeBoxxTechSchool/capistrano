6f29da3d3654da2680a370c4be13cc8b817e8f21
MathieuHoude <mathieu.houde@codeboxx.biz>
2019-07-04 12:23:41 -0400

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@ server "34.203.94.126", user: "ubuntu", roles: %w{app db web}
 # server "db.example.com", user: "deploy", roles: %w{db}
 app = ENV['APP']
 if app.nil? or app.empty?
-  app = "YOUR_FULL_NAME" if  app.empty?
+  app = "YOUR_FULL_NAME" 
 end
 set :application, app
 set :rails_env, "development"
