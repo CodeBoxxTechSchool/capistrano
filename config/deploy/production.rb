d885f24c853e659d766b2125290e7693684c3cca
MathieuHoude <mathieu.houde@codeboxx.biz>
2019-07-04 10:47:35 -0400

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,9 +7,9 @@ server "34.203.94.126", user: "ubuntu", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 app = ENV['APP']
-app = "codeboxx_paul" if  app.empty?
+app = "FULL_NAME" if  app.empty?
 set :application, app
-set :rails_env, "production"
+set :rails_env, "development"
 set :deploy_to, "/home/ubuntu/apps/#{app}"
 set :linked_dirs, %w{tmp/pids tmp/sockets log}
 set :linked_files, %w{config/secrets.yml config/database.yml}
