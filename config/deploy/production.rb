3ca04bb2373ba1121004de2bda5a110b8bbeedcb
Philippe Bouillon <newfylox@users.noreply.github.com>
2019-07-04 12:01:17 -0400

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,9 @@ server "34.203.94.126", user: "ubuntu", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 app = ENV['APP']
-app = "YOUR_FULL_NAME" if  app.empty?
+if app.nil? or app.empty?
+  app = "YOUR_FULL_NAME" if  app.empty?
+end
 set :application, app
 set :rails_env, "development"
 set :bundle_without, "production"
