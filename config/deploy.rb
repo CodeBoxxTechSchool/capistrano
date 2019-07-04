2b5eb55ef1bc9ac6747382caa8047c6e899cf270
MathieuHoude <mathieu.houde@codeboxx.biz>
2019-07-04 10:48:42 -0400

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,8 @@ lock "~> 3.11.0"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
-set :application, "MY-TEAM-LEADER-FIRSTNAME-OR-ELSE"
+set :application, "YOUR_FULL_NAME"
 set :repo_url, "https://github.com/MY-ACCOUNT/MY-REPO.git"
-# set :git_http_username, "username"
-# set :git_http_password, "password"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
